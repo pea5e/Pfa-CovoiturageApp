cdf69b99a60550087745fd93cc51edb1c608d0f1
pea5e <sindibade2011@gmail.com>
2024-12-20 00:42:47 +0100

    Relations Map

---
+++ Config/src/test/java/pfe/emsi/covoiturage/config/ConfigApplicationTests.java
@@ -0,0 +1,13 @@
+package pfe.emsi.covoiturage.config;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
