9b1e1e3a87d62bf6329bab964d9fe382d34e3dba
pea5e <sindibade2011@gmail.com>
2024-12-19 23:28:08 +0100

    Relations Map

---
+++ Notification/src/test/java/pfe/emsi/covoiturage/notification/NotificationApplicationTests.java
@@ -0,0 +1,13 @@
+package pfe.emsi.covoiturage.notification;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NotificationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
---
+++ Voyage/src/test/java/pfe/emsi/covoiturage/voyage/VoyageApplicationTests.java
@@ -0,0 +1,13 @@
+package pfe.emsi.covoiturage.voyage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VoyageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
