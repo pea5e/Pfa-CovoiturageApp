4a0612c2683800736b683908da565129c5c77913
pea5e <sindibade2011@gmail.com>
2025-06-21 15:35:16 +0100

    Relations Map

--- Voyage/src/main/java/pfe/emsi/covoiturage/voyage/Config/FirebaseConfig.java
+++ Voyage/src/main/java/pfe/emsi/covoiturage/voyage/Config/FirebaseConfig.java
@@ -1,28 +1,28 @@
-package pfe.emsi.covoiturage.voyage.Config;
-import java.io.FileInputStream;
-import java.io.IOException;
-
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-import com.google.auth.oauth2.GoogleCredentials;
-import com.google.firebase.FirebaseApp;
-import com.google.firebase.FirebaseOptions;
-
-
-
-@Configuration
-public class FirebaseConfig {
-
-    @Bean
-    public FirebaseApp firebaseApp() throws IOException {
-        FileInputStream serviceAccount =
-            new FileInputStream("src/main/resources/FirebaseSDK.json");
-
-        FirebaseOptions options = new FirebaseOptions.Builder()
-            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
-            .build();
-
-        return FirebaseApp.initializeApp(options);
-    }
-}
+//package pfe.emsi.covoiturage.voyage.Config;
+//import java.io.FileInputStream;
+//import java.io.IOException;
+//
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+//import com.google.auth.oauth2.GoogleCredentials;
+//import com.google.firebase.FirebaseApp;
+//import com.google.firebase.FirebaseOptions;
+//
+//
+//
+//@Configuration
+//public class FirebaseConfig {
+//
+//    @Bean
+//    public FirebaseApp firebaseApp() throws IOException {
+//        FileInputStream serviceAccount =
+//            new FileInputStream("src/main/resources/FirebaseSDK.json");
+//
+//        FirebaseOptions options = new FirebaseOptions.Builder()
+//            .setCredentials(GoogleCredentials.fromStream(serviceAccount))
+//            .build();
+//
+//        return FirebaseApp.initializeApp(options);
+//    }
+//}
